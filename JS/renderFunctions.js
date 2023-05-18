ae114e3d565a1e0fc974a37b7e18b1fc35c28572
Nícolas <48634498+NicolasBispo@users.noreply.github.com>
2023-05-18 12:39:56 -0300

    Mudança no esquema de load

--- JS/renderFunctions.js
+++ JS/renderFunctions.js
@@ -1,16 +1,24 @@
+
 function renderHeader() {
     $("header").load("./header.html");
 }
 
 function renderGerador() {
-    
-    $('.nav-itens li a').click(function (event) {
-        event.preventDefault();
-        let page = $(this).data('pagina');
-        $('#conteudo-exibir').fadeOut(function () {
-            $('#conteudo-exibir').load(page, function () {
-                $('#conteudo-exibir').fadeIn();
+    $(document).ready(function () {
+        $('.nav-itens li a').click(function (event) {
+            event.preventDefault();
+            let page = $(this).data('pagina');
+            
+            $('#conteudo-exibir').fadeOut(function () {
+
+                $('#conteudo-exibir').load(page, function () {
+                    $('#conteudo-exibir').fadeIn();
+                })
             })
-        })        
+        })
     })
+    
+
 }
+
+let loadGenerators = [true, false, false]
